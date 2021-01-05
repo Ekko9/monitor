693e944be2b46fe86f197bffc22d280d6490a9c0
Fsx <45301895+Ekko9@users.noreply.github.com>
2021-01-05 10:20:07 +0800

    Update Check.sh

--- Check.sh
+++ Check.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 cd $(dirname $0)
-user=prouser
 num=80
 ip_list=`cat IP.txt`
 for i in ${ip_list}
 do
-	for j in `./autossh.sh prouser S3userPW1 $i 'df -h' 2>/dev/null | grep /opt |awk  '{print $5}' | sed 's/.$//'`
+	for j in `./autossh.sh user password $i 'df -h' 2>/dev/null | grep /opt |awk  '{print $5}' | sed 's/.$//'`
         do
         	if [ ${j} -gt ${num} ]
         	then
