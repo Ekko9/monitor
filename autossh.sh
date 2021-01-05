390487067b2dfc2a187691be4067672b6f7e8605
Fsx <45301895+Ekko9@users.noreply.github.com>
2021-01-05 10:17:18 +0800

    Update autossh.sh

--- autossh.sh
+++ autossh.sh
@@ -4,7 +4,7 @@
 set timeout 10
 set username [lindex $argv 0]
 set password [lindex $argv 1]
-set realip [lindex $argv 2]
+set realip   [lindex $argv 2]
 set script   [lindex $argv 3]
 
 spawn /usr/bin/ssh -l $username $realip "$script"
